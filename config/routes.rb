1f4468bbb6264dbe5f7e1873e50484b8d2739aad
Martin Sojka <sojka.martin@gmail.com>
2022-02-10 23:30:39 +0100

    Route root to posts.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'welcome#index'
+  root 'posts#index'
 
   resources :posts do
     resources :comments, only: [:index, :create]
